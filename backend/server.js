838ef4252f73263691fc22f68194c2a0ddec333c
Arya-Chakraborty <aryastlawrence@gmail.com>
2025-04-01 15:26:49 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,18 @@ const _dirname = path.resolve();
 
 app.use("/api/v1", funds_dashboard_routes);
 
-app.use(express.static(path.join(_dirname, "/frontend/build")));
+// app.use(express.static(path.join(_dirname, "/frontend/build")));
 
-app.get('*', (_, res) => {
-   res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html")); 
-});
+// app.get('*', (_, res) => {
+//    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html")); 
+// });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`));
